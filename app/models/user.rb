698cafd6741784e75e6e0c71132844fc0e690d06
Pauline Muigai <wanjirumuigai@gmail.com>
2023-04-05 22:12:47 +0300

    done favorite instance method on user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :products, through: :reviews
+
+  def favorite_product
+    highest_rating = self.reviews.map { |review| review.star_rating }.max
+
+    self.reviews.select do |item|
+      item.product if item.star_rating == highest_rating
+    end[
+      0
+    ].product
+  end
 end

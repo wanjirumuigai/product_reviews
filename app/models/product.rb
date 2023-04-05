3caced733cc4a8bd3d6e603e1d609e84bb52cf3b
Pauline Muigai <wanjirumuigai@gmail.com>
2023-04-05 21:45:52 +0300

    done the leave_review instance method on the Review Class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  def leave_review(user, star_rating, comment)
+    Review.create(
+      star_rating: star_rating,
+      comment: comment,
+      product_id: self.id,
+      user_id: user.id
+    )
+  end
 end
